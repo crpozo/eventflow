fb34fe93d305380180e40e00028f44d0d26563e7
Carlos Pozo <carlosricardo.pozo@hotmail.com>
2024-04-16 15:42:32 -0500

    fix font

--- src/views/carrera/detalle/components/Banner.jsx
+++ src/views/carrera/detalle/components/Banner.jsx
@@ -6,7 +6,7 @@ const Banner1 = () => {
       style={{ background: `#faf3e9f7` }}
     >
       <div className="w-full">
-        <h4 className="max-w-full text-xl font-black text-navy-700 md:w-[64%] md:text-[55px] md:leading-[42px] lg:w-[46%] xl:w-[85%] 2xl:w-[75%] 3xl:w-[52%]">
+        <h4 className="max-w-full text-xl font-bold text-navy-700 md:w-[64%] md:text-[55px] md:leading-[42px] lg:w-[46%] xl:w-[85%] 2xl:w-[75%] 3xl:w-[52%]">
           Editar Subárea
         </h4>
         <svg className="pill hidden md:block" width="178" height="170" viewBox="0 0 178 170" fill="none" xmlns="http://www.w3.org/2000/svg">
