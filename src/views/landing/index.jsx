054a2dc793ee496b92c0a9248d6662b5ecf2378e
Carlos Pozo <carlosricardo.pozo@hotmail.com>
2024-04-22 14:31:41 -0500

    Fix creating of event landing relationship

--- src/views/landing/index.jsx
+++ src/views/landing/index.jsx
@@ -14,7 +14,7 @@ import {
 } from "react-icons/ai";
 /* GRAPHQL */
 import { generateClient } from 'aws-amplify/api';
-import { getLanding, getEvent } from '../../graphql/queries';
+import { getEvent, listLandings } from '../../graphql/queries';
 
 export default function SignIn() {
   const { authStatus } = useAuthenticator((context) => [context.authStatus]);
@@ -46,20 +46,33 @@ export default function SignIn() {
   React.useEffect(() => {
 
     async function startData() {
-      const result = await client.graphql({ 
+
+      const resultEvent = await client.graphql({ 
         query: getEvent,
         variables: { id: id } 
       });
-      console.log("GRAPHQL data: ",result.data.getEvent);
-      if(result.data.getEvent){
-          setEvent(result.data.getEvent);
-          setLanding(result.data.getEvent.Landing)
+
+      const resultLanding = await client.graphql({ 
+        query: listLandings,
+        variables: {
+          filter: {
+            landingEventId: {
+              eq: id
+            }
+          }
+        }
+      });
+
+      console.log("GRAPHQL data: ",resultEvent.data.getEvent);
+      if(resultEvent.data.getEvent){
+
+        if(resultLanding.data.listLandings.items[0]) setLanding(resultLanding.data.listLandings.items[0])
 
         // Format price ticket
-        const tickets = result.data.getEvent.Landing.ticketTitle.map((title, index) => {
+        const tickets = resultEvent.data.getEvent.Landing?.ticketTitle.map((title, index) => {
           const cost =
-          result.data.getEvent.Landing.ticketPrice[index] !== undefined
-              ? `$${result.data.getEvent.Landing.ticketPrice[index].toFixed(2)}`
+          resultEvent.data.getEvent.Landing.ticketPrice[index] !== undefined
+              ? `$${resultEvent.data.getEvent.Landing.ticketPrice[index].toFixed(2)}`
               : "Vacio";
           if (index == 0) setSelectedCost(cost);
           return {
@@ -78,8 +91,8 @@ export default function SignIn() {
     }, 1000);
 
   }, []);
-
-  if (loading && landing.length === 0) {
+  
+  if (loading && landing && landing.length === 0) {
     return (
       <div className="fixed bottom-0 left-0 right-0 top-0 z-50 flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-lightPrimary opacity-[100%] p-3">
         <div className="loader mb-4 h-16 w-16 rounded-full border-4 border-t-4 border-gray-200 ease-linear"></div>
@@ -141,108 +154,110 @@ export default function SignIn() {
         </div>
       )}
       {/* Conditionals ==> Register for event +  Checkout and event details*/}
-      <div
-        className={`sm:px-2! container mt-5 mx-auto mb-12 md:mb-15 h-full w-full items-center justify-center px-4 md:px-0 lg:mb-11 lg:items-center lg:justify-start`}
-      >
+      {landing && 
         <div
-          className={` ${
-            showRegister ? "block" : "hidden"
-          } transition-all duration-300`}
+          className={`sm:px-2! container mt-5 mx-auto mb-12 md:mb-15 h-full w-full items-center justify-center px-4 md:px-0 lg:mb-11 lg:items-center lg:justify-start`}
         >
-          <Registro
-            landing={landing}
-            event={event}
-            showRegister = { showRegister }
-            setShowRegister={setShowRegister}
-            userData={userData}
-            setUserData={setUserData}
-            quantityProp={ticketsQuantity}
-            price={selectedCost}
-            eventID={id}
-          />
-        </div>
-        <div className={`${showRegister ? "hidden" : "block"}`}>
           <div
-            className={` mb-[60px] border-b border-gray-300 pb-[60px] transition-all duration-300`}
+            className={` ${
+              showRegister ? "block" : "hidden"
+            } transition-all duration-300`}
           >
-            <h2 className="pt-2 md:pt-0 mb-5 md:!mb-3 text-[28px] md:text-4xl font-bold">Dónde y cuándo</h2>
+            <Registro
+              landing={landing}
+              event={event}
+              showRegister = { showRegister }
+              setShowRegister={setShowRegister}
+              userData={userData}
+              setUserData={setUserData}
+              quantityProp={ticketsQuantity}
+              price={selectedCost}
+              eventID={id}
+            />
+          </div>
+          <div className={`${showRegister ? "hidden" : "block"}`}>
+            <div
+              className={` mb-[60px] border-b border-gray-300 pb-[60px] transition-all duration-300`}
+            >
+              <h2 className="pt-2 md:pt-0 mb-5 md:!mb-3 text-[28px] md:text-4xl font-bold">Dónde y cuándo</h2>
 
-            <div className="grid grid-cols-1 gap-6 sm:gap-5 lg:grid-cols-3">
-              <div className="flex items-center gap-5 lg:col-span-1">
-                <LuCalendarClock className="h-8 w-8 min-w-[31px]" />
-                <div>
-                  <h3 className="text-lg font-bold">Fecha y hora</h3>
-                  {event && <p className="text-md">{formatDateHour(event.date)}</p>}
-                </div>
-              </div>
-              <div className="flex items-center gap-6 lg:ml-[-25px]">
-                <LuMapPin className="h-9 w-9 min-w-[31px]" />
-                <div>
-                  <h3 className="text-lg font-bold">Ubicación</h3>
-                  <p className="text-md max-w-[300px]">{landing.location}</p>
-                </div>
-              </div>
-              {/* Checkout  */}
-              <div className="mx-auto flex w-full min-w-full flex-col justify-center rounded-md border border-gray-500 p-3 md:p-4">
-                <div className="mb-3 flex w-full justify-between gap-5 sm:flex-row sm:items-center">
-                  <select
-                    className="select-arrow w-full md:max-w-[300px] appearance-none text-ellipsis rounded-md border bg-white py-2.5 pl-3 pr-[40px] text-black shadow-sm	outline-none focus:border-indigo-600"
-                    onChange={(e) => {
-                      setSelectedCost(e.target.value);
-                    }}
-                  >
-                    {tickets &&
-                      tickets.map((result, i) => (
-                        <option key={i} value={result.cost}>
-                          {result.title}
-                        </option>
-                      ))}
-                  </select>
-                  {/* Ticket Quantity => Increment / Decrement Boxes  */}
-
-                  <div className="flex shrink-0 items-center gap-2 sm:justify-between">
-                    <div
-                      onClick={quantityDecrementHandler}
-                      className="cursor-pointer rounded-lg bg-[#D9D9D9] bg-opacity-70 p-[8px] text-[#A6A6A6]"
-                    >
-                      <MinusIcon className="text-base" />
-                    </div>
-                    <div className="flex min-w-[25px] items-center justify-center text-xl font-semibold">
-                      {ticketsQuantity}
-                    </div>
-                    <div
-                      onClick={quantityIncrementHandler}
-                      className="cursor-pointer rounded-lg bg-[#D9D9D9] bg-opacity-70 p-[8px] text-[#A6A6A6]"
-                    >
-                      <PlusIcon className="cursor-pointer" />
-                    </div>
+              <div className="grid grid-cols-1 gap-6 sm:gap-5 lg:grid-cols-3">
+                <div className="flex items-center gap-5 lg:col-span-1">
+                  <LuCalendarClock className="h-8 w-8 min-w-[31px]" />
+                  <div>
+                    <h3 className="text-lg font-bold">Fecha y hora</h3>
+                    {event && <p className="text-md">{formatDateHour(event.date)}</p>}
                   </div>
                 </div>
-                <p className="mb-3 text-xl font-semibold">
-                  {selectedCost !== null ? selectedCost : "Vacio"} <span className="text-[15px] font-normal text-[#717171]">+ IVA</span>
-                </p>
-                {/* => Button  */}
-                <button
-                  href="crear"
-                  onClick={() => setShowRegister(true)}
-                  className="linear text-md flex w-full items-center justify-center gap-1 rounded-xl bg-red-500 py-[12px] pl-3 pr-3 font-medium text-white transition duration-200 hover:bg-black"
-                >
-                  Reservar un lugar
-                </button>
+                <div className="flex items-center gap-6 lg:ml-[-25px]">
+                  <LuMapPin className="h-9 w-9 min-w-[31px]" />
+                  <div>
+                    <h3 className="text-lg font-bold">Ubicación</h3>
+                    <p className="text-md max-w-[300px]">{landing.location}</p>
+                  </div>
+                </div>
+                {/* Checkout  */}
+                <div className="mx-auto flex w-full min-w-full flex-col justify-center rounded-md border border-gray-500 p-3 md:p-4">
+                  <div className="mb-3 flex w-full justify-between gap-5 sm:flex-row sm:items-center">
+                    <select
+                      className="select-arrow w-full md:max-w-[300px] appearance-none text-ellipsis rounded-md border bg-white py-2.5 pl-3 pr-[40px] text-black shadow-sm	outline-none focus:border-indigo-600"
+                      onChange={(e) => {
+                        setSelectedCost(e.target.value);
+                      }}
+                    >
+                      {tickets &&
+                        tickets.map((result, i) => (
+                          <option key={i} value={result.cost}>
+                            {result.title}
+                          </option>
+                        ))}
+                    </select>
+                    {/* Ticket Quantity => Increment / Decrement Boxes  */}
+
+                    <div className="flex shrink-0 items-center gap-2 sm:justify-between">
+                      <div
+                        onClick={quantityDecrementHandler}
+                        className="cursor-pointer rounded-lg bg-[#D9D9D9] bg-opacity-70 p-[8px] text-[#A6A6A6]"
+                      >
+                        <MinusIcon className="text-base" />
+                      </div>
+                      <div className="flex min-w-[25px] items-center justify-center text-xl font-semibold">
+                        {ticketsQuantity}
+                      </div>
+                      <div
+                        onClick={quantityIncrementHandler}
+                        className="cursor-pointer rounded-lg bg-[#D9D9D9] bg-opacity-70 p-[8px] text-[#A6A6A6]"
+                      >
+                        <PlusIcon className="cursor-pointer" />
+                      </div>
+                    </div>
+                  </div>
+                  <p className="mb-3 text-xl font-semibold">
+                    {selectedCost !== null ? selectedCost : "Vacio"} <span className="text-[15px] font-normal text-[#717171]">+ IVA</span>
+                  </p>
+                  {/* => Button  */}
+                  <button
+                    href="crear"
+                    onClick={() => setShowRegister(true)}
+                    className="linear text-md flex w-full items-center justify-center gap-1 rounded-xl bg-red-500 py-[12px] pl-3 pr-3 font-medium text-white transition duration-200 hover:bg-black"
+                  >
+                    Reservar un lugar
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="mb-[40px] md:mb-[40px]">
-            <h2 className="mb-5 text-[28px] md:text-4xl font-bold">Informacion adicional</h2>
-            <p className="text-[16px] md:text-lg">{landing.extraInfo}</p>
-          </div>
+            <div className="mb-[40px] md:mb-[40px]">
+              <h2 className="mb-5 text-[28px] md:text-4xl font-bold">Informacion adicional</h2>
+              <p className="text-[16px] md:text-lg">{landing.extraInfo}</p>
+            </div>
 
-          <Link to="mailto:sd@usfq.edu.ec" className="mb-2 flex justify-center text-center text-lg hover:text-red-500">
-            ¿Requiere soporte?
-          </Link>
+            <Link to="mailto:sd@usfq.edu.ec" className="mb-2 flex justify-center text-center text-lg hover:text-red-500">
+              ¿Requiere soporte?
+            </Link>
+          </div>
         </div>
-      </div>
+      }
     </>
   );
 }
